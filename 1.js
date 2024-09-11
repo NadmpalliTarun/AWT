f65f1a7da3ca8befd1741b07b330398d15b5689b
NadimpalliTarun <nadimpallitarun99@gmail.com>
2024-09-11 10:53:19 +0530

    initial commit

---
+++ 1.js
@@ -0,0 +1,20 @@
+let person={
+    name:"taarun",
+    age:18,
+};
+console.log(person.name);
+console.log(Object.entries(person));
+console.log(Object.keys(person));
+let colors=["red","blue"];
+colors[1]="yellow";
+console.log(colors);
+colors.push("red")
+console.log(colors);
+colors.pop()
+console.log(colors);
+colors.shift()
+console.log(colors);
+colors.unshift("black"
+)
+console.log(colors);
+console.log(colors.length)
